5ee01e6ee261640c5347d70c83c30455e6c027de
Theo van Hoesel <tvanhoesel@perceptyx.com>
2023-07-24 14:41:11 +0200

    Bump version to 'v0.102.2'
    Fixed
    - cpanfile dependencies

--- lib/OpenTracing/Implementation/Test.pm
+++ lib/OpenTracing/Implementation/Test.pm
@@ -1,6 +1,6 @@
 package OpenTracing::Implementation::Test;
 
-our $VERSION = 'v0.102.1';
+our $VERSION = 'v0.102.2';
 
 use Moo;
 use aliased 'OpenTracing::Implementation::Test::Tracer';
--- lib/OpenTracing/Implementation/Test/Scope.pm
+++ lib/OpenTracing/Implementation/Test/Scope.pm
@@ -1,6 +1,6 @@
 package OpenTracing::Implementation::Test::Scope;
 
-our $VERSION = 'v0.102.1';
+our $VERSION = 'v0.102.2';
 
 use Moo;
 
